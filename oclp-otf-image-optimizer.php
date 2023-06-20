10c615cc55b2b37f8b1b7f26d1308b9c9d1445d9
Akhyar <ayankcell@gmail.com>
2023-06-20 10:22:06 +0700

    add image converison function

--- oclp-otf-image-optimizer.php
+++ oclp-otf-image-optimizer.php
@@ -2,20 +2,28 @@
 /**
  * Plugin Name: OCLP OTF Image Optimizer
  * Plugin URI: https://www.sadiskon.com
- * Description: Plugin untuk merubah semua gambar di Landing Page WordPress menjadi format WebP dengan memanfaatkan layanan external CDN.  Fungsi utama: convert JPG/PNG ke WebP, Mengurangi beban server hosting untuk convert, CDN, Caching Gambar, membantu memperbaiki OCLP
- # OCLP On The Fly Image Optimizer
-
-Plugin sederhana untuk membantu meng-convert format gambar JPG / PNG ke WebP tanpa membebani server.  Plugin ini memanfaatkan Photon API dari untuk merubah dan mentransformasi gambar dengan mudah.
-
-## Manfaat utama:
-
-- Konversi JPG / PNG / GIF ke Webp
-- Meringankan beban server hosting jika dibandingkan dengan transformasi / conversi menggunakan Image Magic
-- Image caching, menambah ringan gambar ketika dimuat ulang
-- Image CDN gratis.  Gratis selama Photon API masih digratiskan oleh WordPress
-- Hemat Ruang. Plugin ini sangat sederhana, meniru fungsi Photon API pada plugin Jetpack tapi tanpa harus menginstall Jetpack secara penuh.
+ * Description: Plugin untuk merubah semua gambar di Landing Page WordPress menjadi format WebP dengan memanfaatkan layanan external CDN.  Fungsi utama: convert JPG/PNG ke WebP, Mengurangi beban server hosting untuk convert, CDN, Caching Gambar, membantu memperbaiki OCLP.  Plugin tidak berfungsi di localhost
  * Version: 1.0
  * Author: Akhyar
  * Author URI: http://kurajin.com/
  * Text Domain: oclp-otf
  */
+
+ function krj_iwp_content_img( $filtered_image, $context, $attachment_id ) {
+	
+    // 	preg_match('/width="(\d+)/',$filtered_image, $intr_width);
+        
+        $find_pattern = array(
+            '/src=["|\'](https:\/\/|http:\/\/)(.*)(?<!\.wp\.com)(\/.*?)["|\']/',
+            '/srcset=["|\'](https:\/\/|http:\/\/)(.*)(?<!\.wp\.com)(\/.*?)["|\']/'
+        );
+            
+        $replace_pattern = array(
+            'src="//i0.wp.com/${2}${3}"',
+            'srcset="//i0.wp.com/${2}${3}"'
+        );
+        
+        return preg_replace($find_pattern, $replace_pattern, $filtered_image);
+    
+    }
+    add_filter( 'wp_content_img_tag', 'krj_iwp_content_img', 10, 3 );
