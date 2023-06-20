3074b26c366d7055c2bac53d09322a2ac3973a29
Akhyar <ayankcell@gmail.com>
2023-06-20 09:45:24 +0700

    rename readme.md

---
+++ oclp-otf-image-optimizer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: OCLP OTF Image Optimizer
+ * Plugin URI: https://www.sadiskon.com
+ * Description: Plugin untuk merubah semua gambar di Landing Page WordPress menjadi format WebP dengan memanfaatkan layanan external CDN.  Fungsi utama: convert JPG/PNG ke WebP, Mengurangi beban server hosting untuk convert, CDN, Caching Gambar, membantu memperbaiki OCLP
+ # OCLP On The Fly Image Optimizer
+
+Plugin sederhana untuk membantu meng-convert format gambar JPG / PNG ke WebP tanpa membebani server.  Plugin ini memanfaatkan Photon API dari untuk merubah dan mentransformasi gambar dengan mudah.
+
+## Manfaat utama:
+
+- Konversi JPG / PNG / GIF ke Webp
+- Meringankan beban server hosting jika dibandingkan dengan transformasi / conversi menggunakan Image Magic
+- Image caching, menambah ringan gambar ketika dimuat ulang
+- Image CDN gratis.  Gratis selama Photon API masih digratiskan oleh WordPress
+- Hemat Ruang. Plugin ini sangat sederhana, meniru fungsi Photon API pada plugin Jetpack tapi tanpa harus menginstall Jetpack secara penuh.
+ * Version: 1.0
+ * Author: Akhyar
+ * Author URI: http://kurajin.com/
+ * Text Domain: oclp-otf
+ */
