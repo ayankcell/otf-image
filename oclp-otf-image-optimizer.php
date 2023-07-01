58e59c45512e8e2f5878db5cee37a2bf83e80947
Akhyar <ayankcell@gmail.com>
2023-07-01 12:35:59 +0700

    change pattern

--- oclp-otf-image-optimizer.php
+++ oclp-otf-image-optimizer.php
@@ -1,27 +1,17 @@
 <?php
 /**
  * Plugin Name: OCLP OTF Image Optimizer
- * Plugin URI: https://www.kurajin.com
+ * Plugin URI: https://www.sadiskon.com
  * Description: Plugin untuk merubah semua gambar di Landing Page WordPress menjadi format WebP dengan memanfaatkan layanan external CDN.  Fungsi utama: convert JPG/PNG ke WebP, Mengurangi beban server hosting untuk convert, CDN, Caching Gambar, membantu memperbaiki OCLP.  Plugin tidak berfungsi di localhost
- * Version: 1.0
+ * Version: 1.5
  * Author: Akhyar
- * Author URI: https://www.kurajin.com/
+ * Author URI: http://kurajin.com/
  * Text Domain: oclp-otf
  */
 
  function krj_iwp_content_img( $filtered_image, $context, $attachment_id ) {
-	
-    // 	preg_match('/width="(\d+)/',$filtered_image, $intr_width);
-        
-        $find_pattern = array(
-            '/src=["|\'](https:\/\/|http:\/\/)(.*)(?<!\.wp\.com)(\/.*?)["|\']/',
-            '/srcset=["|\'](https:\/\/|http:\/\/)(.*)(?<!\.wp\.com)(\/.*?)["|\']/'
-        );
-            
-        $replace_pattern = array(
-            'src="//i0.wp.com/${2}${3}"',
-            'srcset="//i0.wp.com/${2}${3}"'
-        );
+		$find_pattern = '/(https:\/\/|https:\/\/)(?<!\.wp\.com)([^\s]+\.)(jpe?g|png|gif)/';
+	 	$replace_pattern= '//i'.rand(0,3).'.wp.com/${2}${3}';
         
         return preg_replace($find_pattern, $replace_pattern, $filtered_image);
     
